997ef77d954ef1696a4ff6fe793b2d541e498ec6
gogojojo <o8oxoxxxxxx@icloud.com>
2025-06-07 14:27:35 +0900

    Add IMEI input form

--- app/controllers/imei_checks_controller.rb
+++ app/controllers/imei_checks_controller.rb
@@ -3,5 +3,11 @@ class ImeiChecksController < ApplicationController
   end
 
   def create
+    imeis = params[:imeis].to_s.spilit(/\r?\n/).map(&:strip).reject(&:blank?)
+    @result = ImeiCheckService.check_multiple(imeis)
+
+    @risults = imeis.map { |imei| [imei, "-"]}
+
+    render :index
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
   root "imei_checks#index"
-  resouces :imei_checks, only: [:index, :create]
+  resource :imei_checks, only: [:index, :create]
 end
