c13d98065196be42f09c3faa5b25a3b0d5774e37
gogojojo <o8oxoxxxxxx@icloud.com>
2025-06-15 18:41:27 +0900

    Regenerate bin/bundle for Render with bundler 2.3.27

--- .render-build.sh
+++ .render-build.sh
@@ -5,3 +5,11 @@ yarn install
 
 echo "🌐 Installing Puppeteer Chromium..."
 yarn exec puppeteer browsers install chrome
+
+
+# PuppeteerのChromiumインストール
+yarn install
+yarn exec puppeteer browsers install chrome
+
+# バンドラーのbinstubが壊れている場合に備えて再生成
+bundle binstubs bundler --force
