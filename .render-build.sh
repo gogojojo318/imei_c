d2df25755c7f2430b3073c15475cf8cc3fb6c092
gogojojo <o8oxoxxxxxx@icloud.com>
2025-06-17 13:44:02 +0900

    修正

--- .render-build.sh
+++ .render-build.sh
@@ -10,9 +10,3 @@ export PUPPETEER_DOWNLOAD_PATH=/opt/render/.cache/puppeteer
 
 echo "Installing Chromium for Puppeteer..."
 npx puppeteer browsers install chrome
-
-echo "Setting CHROME_EXECUTABLE_PATH..."
-export CHROME_EXECUTABLE_PATH=$(node -e "console.log(require('puppeteer').executablePath())")
-
-echo "Starting app..."
-node scripts/au_checker.js
